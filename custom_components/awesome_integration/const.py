4d52b913272c89d7b8be91eb1b10506ec8cdbc22
Spike <spike@example.com>
2026-07-08 19:10:28 -0600

    Initial: generated from blueprint, renamed to awesome_integration

---
+++ custom_components/awesome_integration/const.py
@@ -0,0 +1,8 @@
+"""Constants for awesome_integration."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "awesome_integration"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
